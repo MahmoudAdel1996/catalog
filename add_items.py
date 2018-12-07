84891f030992c71c1b452ce5e6d62bf59de11b6f
unknown <mahmoooud.adel@gmail.com>
2018-12-07 10:18:34 +0200

    starter code

--- add_items.py
+++ add_items.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('sqlite:///products.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
